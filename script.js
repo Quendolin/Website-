1311942cc65db92fd57c7935e55692f9af37888f
Quendolin <97144002+Quendolin@users.noreply.github.com>
2024-09-01 16:13:43 +0200

    e

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("first");
+
+button.addEventListener("click", function() {
+    const ausgabe = document.getElementById("test");
+    ausgabe.textContent = "Trololooloo";
+});
