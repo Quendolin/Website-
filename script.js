b5a0ae51c5ea5bd2420c7cade58331da2a8c319e
Quendolin <97144002+Quendolin@users.noreply.github.com>
2024-11-08 20:43:02 +0100

    d

--- script.js
+++
@@ -1,6 +0,0 @@
-const button = document.getElementById("first");
-
-button.addEventListener("click", function() {
-    const ausgabe = document.getElementById("test");
-    ausgabe.textContent = "Trololooloo";
-});
